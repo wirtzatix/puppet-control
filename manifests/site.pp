e06eb62b2220e948786675ca84a4ea25b2d7baff
root <root@andywirtz4.mylabserver.com>
2018-06-28 11:24:59 +0000

    test side.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,12 +25,6 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node 'andywirtz5.mylabserver.com' {
-  notify { 'test-message-for-andywirtz5':
-    message => 'this is andywirtz5 using a node def',
-  }
-}
-
 
 node 'andywirtz4.mylabserver.com' {
   include pe_repo::platform::el_6_x86_64
